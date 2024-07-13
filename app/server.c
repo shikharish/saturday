f92bba0d4a547748d4fb3752a5820bfbb7e3aa7d
Shikhar <shikharish05@gmail.com>
2024-07-13 23:31:33 +0530

    concurrent requests
    Signed-off-by: Shikhar <shikharish05@gmail.com>

--- app/server.c
+++ app/server.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,8 +10,59 @@
 
 #define BUF_SIZE 1024
 
-char *resp_ok = "HTTP/1.1 200 OK\r\n\r\n";
-char *resp_not_found = "HTTP/1.1 404 Not Found\r\n\r\n";
+const char *resp_ok = "HTTP/1.1 200 OK\r\n\r\n";
+const char *resp_not_found = "HTTP/1.1 404 Not Found\r\n\r\n";
+int server_fd;
+
+void *handle_reqs(void *fd) {
+	int client_fd = *((int *)fd);
+	char req_buf[BUF_SIZE] = {0};
+	if (read(client_fd, req_buf, sizeof(req_buf)) < 0) {
+		printf("read request failed\n");
+		goto error;
+	}
+
+	char *tmp = strdup(req_buf);
+	char *path = strtok(tmp, " ");
+	path = strtok(NULL, " ");
+
+	const char *resp = NULL;
+	if (strstr(path, "/user-agent")) {
+		char *user_agent = strstr(req_buf, "User-Agent");
+		printf("%s\n", user_agent);
+		user_agent += 12;
+		char resp_user_agent[BUF_SIZE] = {0};
+		sprintf(resp_user_agent,
+				"HTTP/1.1 200 OK\r\nContent-Type: "
+				"text/plain\r\nContent-Length: %lu\r\n\r\n%s",
+				strlen(user_agent) - 4, user_agent);
+		resp = resp_user_agent;
+		printf("%s\n", resp);
+	} else if (strstr(path, "/echo/")) {
+		char *str = strtok(path, "/");
+		str = strtok(NULL, "/");
+		char resp_echo[BUF_SIZE] = {0};
+		sprintf(resp_echo,
+				"HTTP/1.1 200 OK\r\nContent-Type: "
+				"text/plain\r\nContent-Length: %lu\r\n\r\n%s",
+				strlen(str), str);
+		resp = resp_echo;
+	} else if (strcmp(path, "/") == 0) {
+		resp = resp_ok;
+	} else {
+		resp = resp_not_found;
+	}
+
+	if (send(client_fd, resp, strlen(resp), 0) < 0) {
+		printf("send response failed\n");
+		goto error;
+	}
+	return NULL;
+
+error:
+	close(server_fd);
+	return NULL;
+}
 
 int main() {
 	// Disable output buffering
@@ -18,10 +70,6 @@ int main() {
 	setbuf(stderr, NULL);
 
 	printf("Logs from your program will appear here!\n");
-
-	int server_fd;
-	unsigned int client_addr_len;
-	struct sockaddr_in client_addr;
 
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
@@ -55,58 +103,23 @@ int main() {
 		printf("Listen failed: %s \n", strerror(errno));
 		goto error;
 	}
-
 	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
 
-	int client_fd =
-		accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-	if (client_fd < 0) {
-		printf("accept failed\n");
-		goto error;
-	}
-	printf("Client connected\n");
+	while (1) {
+		unsigned int client_addr_len;
+		struct sockaddr_in client_addr;
 
-	char req_buf[BUF_SIZE] = {0};
-	if (read(client_fd, req_buf, sizeof(req_buf)) < 0) {
-		printf("read request failed\n");
-		goto error;
-	}
+		client_addr_len = sizeof(client_addr);
 
-	char *tmp = strdup(req_buf);
-	char *path = strtok(tmp, " ");
-	path = strtok(NULL, " ");
-
-	char *resp = NULL;
-	if (strstr(path, "/user-agent")) {
-		char *user_agent = strstr(req_buf, "User-Agent");
-		printf("%s\n", user_agent);
-		user_agent += 12;
-		char resp_user_agent[BUF_SIZE] = {0};
-		sprintf(resp_user_agent,
-				"HTTP/1.1 200 OK\r\nContent-Type: "
-				"text/plain\r\nContent-Length: %lu\r\n\r\n%s",
-				strlen(user_agent) - 4, user_agent);
-		resp = resp_user_agent;
-		printf("%s\n", resp);
-	} else if (strstr(path, "/echo/")) {
-		char *str = strtok(path, "/");
-		str = strtok(NULL, "/");
-		char resp_echo[BUF_SIZE] = {0};
-		sprintf(resp_echo,
-				"HTTP/1.1 200 OK\r\nContent-Type: "
-				"text/plain\r\nContent-Length: %lu\r\n\r\n%s",
-				strlen(str), str);
-		resp = resp_echo;
-	} else if (strcmp(path, "/") == 0) {
-		resp = resp_ok;
-	} else {
-		resp = resp_not_found;
-	}
-
-	if (send(client_fd, resp, strlen(resp), 0) < 0) {
-		printf("send response failed\n");
-		goto error;
+		int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+							   &client_addr_len);
+		if (client_fd < 0) {
+			printf("accept failed\n");
+			goto error;
+		}
+		pthread_t id;
+		pthread_create(&id, NULL, handle_reqs, &client_fd);
+		printf("Client connected - %d\n", client_fd);
 	}
 
 	close(server_fd);
